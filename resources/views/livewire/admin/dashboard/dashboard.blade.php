4adc0d398921b7a07142afb0c11bcbe00824395b
salialkhayzel <111584408+salialkhayzel@users.noreply.github.com>
2024-04-19 20:07:07 +0800

    dashboard

--- resources/views/livewire/admin/dashboard/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard/dashboard.blade.php
@@ -117,9 +117,6 @@
                     </div>
                 </div>
             </div>
-        </div>
-
-        <div class="row">
             <div class="col-12 col-xl-12 stretch-card">
                 <div class="row flex-grow-1">
                 <div class="col-md-4 grid-margin stretch-card">
@@ -220,9 +217,6 @@
                     </div>
                 </div>
             </div>
-        </div>
-
-        <div class="row">
             <div class="col-12 col-xl-12 grid-margin stretch-card">
                 <div class="card overflow-hidden">
                     <div class="card-body">
@@ -254,7 +248,8 @@
                     </div>
                 </div>
             </div>
-        </div> 
+        </div>
+
     </div>
 
     <script src="../assets/vendors/core/core.js"></script>
